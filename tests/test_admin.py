e0fffa4d9d565ceb325353817b7e748c7fd5d28f
mongkok <dani.pyc+github@gmail.com>
2017-04-26 18:28:39 +0800

    Remove test admin asserts

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -18,32 +18,16 @@ class AdminTests(TestCase):
 
     def test_admin_country(self):
         model_admin = admin.CountryAdmin(models.Country, self.site)
-
         self.assertIn('cca2', model_admin.get_fields(request))
-        self.assertEqual(
-            models.Country.objects.count(),
-            model_admin.get_queryset(request).count())
 
     def test_admin_currency(self):
         model_admin = admin.CurrencyAdmin(models.Currency, self.site)
-
         self.assertIn('code', model_admin.get_fields(request))
-        self.assertEqual(
-            models.Currency.objects.count(),
-            model_admin.get_queryset(request).count())
 
     def test_admin_language(self):
         model_admin = admin.LanguageAdmin(models.Language, self.site)
-
         self.assertIn('code', model_admin.get_fields(request))
-        self.assertEqual(
-            models.Language.objects.count(),
-            model_admin.get_queryset(request).count())
 
     def test_admin_translation(self):
         model_admin = admin.TranslationAdmin(models.Translation, self.site)
-
         self.assertIn('language', model_admin.get_fields(request))
-        self.assertEqual(
-            models.Translation.objects.count(),
-            model_admin.get_queryset(request).count())
