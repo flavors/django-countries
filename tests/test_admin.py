ef20c2db1c21bee2cf16f97d3fd3354c90f226ae
mongkok <dani.pyc+github@gmail.com>
2017-04-26 16:41:22 +0800

    test admin args

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -17,9 +17,7 @@ class AdminTests(TestCase):
         self.site = AdminSite()
 
     def test_admin_country(self):
-        model_admin = admin.CountryAdmin(
-            model=models.Country,
-            admin_site=self.site)
+        model_admin = admin.CountryAdmin(models.Country, self.site)
 
         self.assertIn('cca2', model_admin.get_fields(request))
         self.assertEqual(
@@ -27,9 +25,7 @@ class AdminTests(TestCase):
             model_admin.get_queryset(request).count())
 
     def test_admin_currency(self):
-        model_admin = admin.CurrencyAdmin(
-            model=models.Currency,
-            admin_site=self.site)
+        model_admin = admin.CurrencyAdmin(models.Currency, self.site)
 
         self.assertIn('code', model_admin.get_fields(request))
         self.assertEqual(
@@ -37,9 +33,7 @@ class AdminTests(TestCase):
             model_admin.get_queryset(request).count())
 
     def test_admin_language(self):
-        model_admin = admin.LanguageAdmin(
-            model=models.Language,
-            admin_site=self.site)
+        model_admin = admin.LanguageAdmin(models.Language, self.site)
 
         self.assertIn('code', model_admin.get_fields(request))
         self.assertEqual(
@@ -47,9 +41,7 @@ class AdminTests(TestCase):
             model_admin.get_queryset(request).count())
 
     def test_admin_translation(self):
-        model_admin = admin.TranslationAdmin(
-            model=models.Translation,
-            admin_site=self.site)
+        model_admin = admin.TranslationAdmin(models.Translation, self.site)
 
         self.assertIn('language', model_admin.get_fields(request))
         self.assertEqual(
