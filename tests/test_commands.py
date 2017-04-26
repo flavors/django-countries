e21239a7f5cc7304332107045f66baa304069a9e
mongkok <dani.pyc+github@gmail.com>
2017-04-26 16:09:36 +0800

    remove ci py3.3

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -5,4 +5,4 @@ from django.test import TestCase
 class CommandsTests(TestCase):
 
     def test_command_countries_setup(self):
-        call_command('countries_setup', countries='VN')
+        call_command('countries_setup', countries='AD,VN')
