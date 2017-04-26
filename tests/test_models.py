68bcfe286c00c6dac4edc579c9b170b3fec2f84a
mongkok <dani.pyc+github@gmail.com>
2017-04-26 14:53:16 +0800

    test models

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,9 +1,41 @@
+import random
+import string
+
+from django.contrib.gis import geos
 from django.test import TestCase
 
-from countries_flavor.models import Country
+from countries_flavor import models
+
+
+def random_code(choices, length):
+    return ''.join(random.choice(choices) for i in range(length))
 
 
 class ModelsTests(TestCase):
 
-    def test_models_country(self):
-        Country.objects.create(cca2='HK')
+    def test_models_countries_str(self):
+        country_cca2 = random_code(string.ascii_uppercase, 2)
+        country = models.Country.objects.create(
+            cca2=country_cca2,
+            location=geos.Point(0, 1),
+            landlocked=True,
+            alt_spellings=[],
+            calling_codes=[],
+            tlds=[])
+
+        self.assertEqual(str(country), country_cca2)
+
+        language_code = random_code(string.ascii_lowercase, 3)
+        language = models.Language.objects.create(code=language_code)
+        self.assertEqual(str(language), language_code)
+
+        translation = models.Translation.objects.create(
+            country=country,
+            language=language)
+
+        self.assertTrue(str(translation).startswith(country_cca2))
+
+    def test_models_currency_str(self):
+        currency_code = random_code(string.ascii_uppercase, 3)
+        currency = models.Currency.objects.create(code=currency_code)
+        self.assertEqual(str(currency), currency_code)
