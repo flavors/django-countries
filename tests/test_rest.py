99d937d22803d20d43e77946fa4f6a6d0586c15f
mongkok <dani.pyc+github@gmail.com>
2017-04-27 03:13:16 +0800

    Tests drf view sets

--- tests/test_rest.py
+++ tests/test_rest.py
@@ -1,6 +1,8 @@
 import string
 
 from django.contrib.gis import geos
+from django.urls import reverse
+
 from rest_framework.test import APITestCase
 
 from countries_flavor import models
@@ -21,6 +23,18 @@ class RestFrameworkTests(APITestCase):
                 'tlds': []
             })
 
-    def test_country_list(self):
-        response = self.client.get('/countries')
-        print(response.data)
+    def test_country_list_filter(self):
+        response = self.client.get(reverse('countries:country-list'), {
+            'cca2': self.country.cca2
+        })
+
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['cca2'], self.country.cca2)
+
+    def test_country_detail(self):
+        response = self.client.get(reverse(
+            'countries:country-detail', kwargs={
+                'pk': self.country.cca2
+            }))
+
+        self.assertEqual(response.data['cca2'], self.country.cca2)
