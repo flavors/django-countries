233fdfd1a6929d0d3d8c1103fdec533fd8ca2103
mongkok <dani.pyc+github@gmail.com>
2017-04-26 16:07:12 +0800

    test models get_or_create

--- tests/test_models.py
+++ tests/test_models.py
@@ -15,21 +15,24 @@ class ModelsTests(TestCase):
 
     def test_models_countries_str(self):
         country_cca2 = random_code(string.ascii_uppercase, 2)
-        country = models.Country.objects.create(
+        country, _ = models.Country.objects.get_or_create(
             cca2=country_cca2,
-            location=geos.Point(0, 1),
-            landlocked=True,
-            alt_spellings=[],
-            calling_codes=[],
-            tlds=[])
+            defaults={
+                'location': geos.Point(0, 1),
+                'landlocked': True,
+                'alt_spellings': [],
+                'calling_codes': [],
+                'tlds': []
+            })
 
         self.assertEqual(str(country), country_cca2)
 
         language_code = random_code(string.ascii_lowercase, 3)
-        language = models.Language.objects.create(code=language_code)
+        language, _ = models.Language.objects\
+            .get_or_create(code=language_code)
         self.assertEqual(str(language), language_code)
 
-        translation = models.Translation.objects.create(
+        translation, _ = models.Translation.objects.get_or_create(
             country=country,
             language=language)
 
@@ -37,5 +40,6 @@ class ModelsTests(TestCase):
 
     def test_models_currency_str(self):
         currency_code = random_code(string.ascii_uppercase, 3)
-        currency = models.Currency.objects.create(code=currency_code)
+        currency, _ = models.Currency.objects\
+            .get_or_create(code=currency_code)
         self.assertEqual(str(currency), currency_code)
