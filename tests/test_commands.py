5cc38705e6f05c5df54bb4c968c1407866b42aba
mongkok <dani.pyc+github@gmail.com>
2017-04-26 22:54:19 +0800

    Test command

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -7,14 +7,14 @@ from countries_flavor import models
 class CommandsTests(TestCase):
 
     def test_command_countries_setup(self):
-        countries = ('AD', 'FR')
+        countries = ('AD', 'FR', 'XK')
         call_command('countries_setup', countries=','.join(countries))
 
         manager = models.Country.objects
-        self.assertEqual(manager.filter(cca2__in=countries).count(), 2)
 
-        border = manager.get(cca2=countries[0]).borders.get(cca2=countries[1])
-        self.assertTrue(border.cca2, countries[1])
+        self.assertEqual(
+            manager.filter(cca2__in=countries).count(),
+            len(countries))
 
-        border = manager.get(cca2=countries[1]).borders.get(cca2=countries[0])
-        self.assertTrue(border.cca2, countries[0])
+        border = manager.get(cca2='AD').borders.get(cca2='FR')
+        self.assertTrue(border.cca2, 'FR')
