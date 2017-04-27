e0a152b13b111663c1c3f385fd21b5598f6af80f
mongkok <dani.pyc+github@gmail.com>
2017-04-28 00:53:09 +0800

    Add app_name to drf urls

--- countries_flavor/rest_framework/urls.py
+++ countries_flavor/rest_framework/urls.py
@@ -2,7 +2,7 @@ from rest_framework.routers import SimpleRouter
 
 from . import views
 
-
+app_name = 'countries_flavor.rest_framework'
 router = SimpleRouter(trailing_slash=False)
 router.register(r'countries', views.CountryViewSet)
 
