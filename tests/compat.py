c92c8ac57d9c58b5a42ada2cc7784bdd308a7a08
mongkok <dani.pyc+github@gmail.com>
2017-04-27 23:11:51 +0800

    Add test compat

--- tests/compat.py
+++ tests/compat.py
@@ -1,9 +1,16 @@
 """
-This file exists to contain all Django and Python compatibility issues.
-In order to avoid circular references, nothing should be imported from suit lib.
+The `compat` module provides support for backwards compatibility with older
+versions of Django/Python, and compatibility wrappers around optional packages.
 """
+import django
 
 try:
     from django.urls import reverse
 except ImportError:  # Django < 1.10
     from django.core.urlresolvers import reverse
+
+
+def get_postgres_engine():
+    if django.VERSION >= (2, 0):
+        return 'django.db.backends.postgresql'
+    return 'django.contrib.gis.db.backends.postgis'
