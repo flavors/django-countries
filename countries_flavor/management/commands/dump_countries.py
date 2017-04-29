9aee7472159c901a43624e5568c9f3133587f38f
mongkok <dani.pyc+github@gmail.com>
2017-04-29 23:33:48 +0800

    Command improvs

--- countries_flavor/management/commands/dump_countries.py
+++ countries_flavor/management/commands/dump_countries.py
@@ -1,6 +1,6 @@
 import os
 
-from django.core import serializers
+from django.core.management import call_command
 
 from ... import models
 from ._base_dumper import DumperBaseCommand
@@ -10,21 +10,44 @@ class Command(DumperBaseCommand):
     help = 'Dump all data'
 
     def handle(self, **options):
-        all_path = os.path.join(self.rootdir, 'all')
+        self.dump_all()
+        self.dump_borders()
 
-        for fixture in os.listdir(all_path):
-            model = os.path.splitext(fixture)[0]
-            self.dumpdata(model, os.path.join(all_path, fixture))
+        # skip borders field serialize
+        models.Country._meta.many_to_many = [
+            field for field in models.Country._meta.many_to_many
+            if field.attname != 'borders'
+        ]
 
         for country in models.Country.objects.all():
-            path = "{path}/countries/{country}.geo.json".format(
-                country=country.cca2.lower(),
-                path=self.rootdir)
+            self.dump_country(country)
 
-            with open(path, 'w') as fixture:
-                fixture.write(serializers.serialize('json', [country]))
+    @classmethod
+    def dumpdata(cls, model_name, path):
+        model = "countries_flavor.{model}".format(model=model_name)
+        call_command('dumpdata', model, '--output', path)
 
-            if country.divisions.exists():
-                with open(path.replace('geo', 'divisions'), 'w') as fixture:
-                    fixture.write(serializers.serialize(
-                        'json', country.divisions.all()))
+    def dump_all(self):
+        all_dir = os.path.join(self.rootdir, 'all')
+
+        for fixture in os.listdir(all_dir):
+            model = os.path.splitext(fixture)[0]
+            self.dumpdata(model, os.path.join(all_dir, fixture))
+
+    def dump_borders(self):
+        with self.open_fixture('m2m/borders', 'w') as fixture:
+            fixture.write(models.Country.objects.all(), fields=('borders',))
+
+    def dump_country(self, country):
+        path = "countries/{cca2}.geo".format(cca2=country.cca2.lower())
+
+        with self.open_fixture(path, 'w') as fixture:
+            fixture.write([country])
+
+        for related_field in ('divisions', 'names'):
+            related_manager = getattr(country, related_field)
+
+            if related_manager.exists():
+                related_path = path.replace('geo', related_field)
+                with self.open_fixture(related_path, 'w') as fixture:
+                    fixture.write(related_manager.all())
--- countries_flavor/management/commands/load_countries.py
+++ countries_flavor/management/commands/load_countries.py
@@ -1,6 +1,8 @@
 import os
 
 from django.core.management import call_command
+
+from ... import models
 from ._base_dumper import DumperBaseCommand
 
 
@@ -8,7 +10,21 @@ class Command(DumperBaseCommand):
     help = 'Load fixtures'
 
     def handle(self, **options):
-        for root, subdirs, files in os.walk(self.rootdir):
+        fixtures = []
+
+        for root, dirs, files in os.walk(self.rootdir, topdown=True):
+            if 'm2m' in dirs:
+                dirs.remove('m2m')
+
             for fixture in files:
-                call_command('loaddata', os.path.join(
+                fixtures.append(os.path.join(
                     root.split('fixtures/')[1], fixture))
+
+        for fixture in sorted(fixtures, key=lambda path: any(
+                f in path for f in ('all/locale', 'divisions', 'names'))):
+            call_command('loaddata', fixture)
+
+        with self.open_fixture('m2m/borders', 'r') as fixture:
+            for data in fixture.read():
+                country = models.Country.objects.get(cca2=data.object.pk)
+                country.borders.add(*data.m2m_data['borders'])
