472492c8698954bbb7ce9d7a309ab4f97a93ffc0
mongkok <dani.pyc+github@gmail.com>
2017-04-30 00:51:40 +0800

    Add models test cases

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,4 @@
 import string
-
-from django.contrib.gis import geos
 from django.test import TestCase
 
 from countries_flavor import models
@@ -9,33 +7,47 @@ from .helpers import random_code
 
 class ModelsTests(TestCase):
 
+    def test_models_continent_str(self):
+        continent_code = random_code(string.ascii_uppercase, 3)
+        continent = models.Continent(code=continent_code)
+        self.assertEqual(str(continent), continent_code)
+
     def test_models_countries_str(self):
         country_cca2 = random_code(string.ascii_uppercase, 2)
-        country, _ = models.Country.objects.get_or_create(
-            cca2=country_cca2,
-            defaults={
-                'location': geos.Point(0, 1),
-                'landlocked': True,
-                'alt_spellings': [],
-                'calling_codes': [],
-                'tlds': []
-            })
-
+        country = models.Country(cca2=country_cca2)
         self.assertEqual(str(country), country_cca2)
 
         language_code = random_code(string.ascii_lowercase, 3)
-        language, _ = models.Language.objects\
-            .get_or_create(cla3=language_code)
+        language = models.Language(cla3=language_code)
         self.assertEqual(str(language), language_code)
 
-        translation, _ = models.CountryTranslation.objects.get_or_create(
+        country_name = models.CountryTranslation(
             country=country,
             language=language)
 
-        self.assertTrue(str(translation).startswith(country_cca2))
+        self.assertTrue(str(country_name).startswith(country_cca2))
+
+        division = models.Division(country=country)
+        self.assertTrue(str(division).startswith(country_cca2))
 
     def test_models_currency_str(self):
         currency_code = random_code(string.ascii_uppercase, 3)
-        currency, _ = models.Currency.objects\
-            .get_or_create(code=currency_code)
+        currency = models.Currency(code=currency_code)
         self.assertEqual(str(currency), currency_code)
+
+    def test_models_locale_str(self):
+        locale_code = random_code(string.ascii_letters, 2)
+        locale = models.Locale(code=locale_code)
+        self.assertEqual(str(locale), locale_code)
+
+    def test_models_timezone_str(self):
+        timezone_name = random_code(string.ascii_letters, 16)
+        timezone = models.Locale(code=timezone_name)
+        self.assertEqual(str(timezone), timezone_name)
+
+    def test_models_translation_str(self):
+        locale_code = random_code(string.ascii_letters, 2)
+        locale = models.Locale(code=locale_code)
+
+        translation = models.Translation(locale=locale, content=locale)
+        self.assertTrue(str(translation).startswith(str(locale)))
