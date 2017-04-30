97f4f64facffd71991117e98a0ccf900196cc136
mongkok <dani.pyc+github@gmail.com>
2017-05-01 01:16:26 +0800

    Add locale manager

---
+++ countries_flavor/managers.py
@@ -0,0 +1,19 @@
+import re
+
+from django.db import models
+from .shortcuts import get_model
+
+
+class LocaleManager(models.Manager):
+
+    def create_locale(self, code, regex=re.compile(r'.*_([A-Z]{2})$')):
+        language = get_model('language').objects.get(cla2=code[:2])
+        country_match = regex.match(code)
+
+        if country_match is not None:
+            country = get_model('country').objects\
+                .get(cca2=country_match.group(1))
+        else:
+            country = None
+
+        return self.create(code=code, language=language, country=country)
