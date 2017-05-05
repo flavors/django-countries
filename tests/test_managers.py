777573cfd686bd0d03addfef15c1983e6c7711ee
mongkok <dani.pyc+github@gmail.com>
2017-05-05 11:21:39 +0200

    Add locale manager test case

--- tests/test_managers.py
+++ tests/test_managers.py
@@ -10,6 +10,17 @@ from .fixtures import random_code
 class ManagersTests(TestCase):
 
     def test_managers_create_locale(self):
+        language = models.Language.objects.create(
+            cla2=random_code(string.ascii_lowercase, 2)
+        )
+
+        locale = models.Locale.objects.create_locale(code=language.cla2)
+
+        self.assertIsNone(locale.country)
+        self.assertEqual(locale.language, language)
+        self.assertEqual(locale.code, language.cla2)
+
+    def test_managers_create_country_locale(self):
         country, _ = get_or_create_country()
 
         language = models.Language.objects.create(
