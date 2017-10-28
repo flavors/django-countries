3294c35daab66f841773bc3cf2704d2f4ce7efe4
mongkok <domake.io@gmail.com>
2017-10-28 12:40:48 +0530

    Remove deprecated tests

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,5 +1,3 @@
-from unittest import mock
-
 from django.core.management import call_command
 from django.test import TestCase
 
@@ -7,24 +5,6 @@ from countries import models
 
 
 class CommandsTests(TestCase):
-
-    @mock.patch('countries.management.commands.collect_countries'
-                '.Command.request', new=lambda cls, path: [])
-    def test_command_collect_countries(self):
-        call_command('collect_countries')
-
-    def test_command_collected_countries_list(self):
-        countries = ('AD', 'FR', 'XK')
-        call_command('collect_countries', countries=','.join(countries))
-
-        manager = models.Country.objects
-
-        self.assertEqual(
-            manager.filter(cca2__in=countries).count(),
-            len(countries))
-
-        border = manager.get(cca2='AD').borders.get(cca2='FR')
-        self.assertTrue(border.cca2, 'FR')
 
     def test_command_load_and_dumpcountries(self):
         call_command('loadcountries', babel=True, verbosity=0)
